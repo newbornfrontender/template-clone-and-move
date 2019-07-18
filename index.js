54bac41cc30117211e795e313dfb1dbc432342d0
Infant Frontender <newbornfrontender@gmail.com>
2019-07-18 09:53:15 +0300

    chore: check retrigger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const template = document.querySelector('#template');
-const templateContent = template.content.querySelector('h1');
+const templateContent = template.content.querySelector('.title');
+
 const templateClone = document.importNode(templateContent, true);
 
 const switcher = document.querySelector('.switcher');
@@ -10,10 +11,22 @@ const secondWrapper = wrappers[1];
 
 firstWrapper.appendChild(templateClone);
 
+function trigger(targetClass, color) {
+  const addedTemplateContent = document.querySelector(`.${targetClass}`);
+
+  addedTemplateContent.addEventListener('click', function() {
+    this.attributeStyleMap.set('color', color);
+  });
+};
+
+trigger('title', 'red');
+
 switcher.addEventListener('click', () => {
   while (firstWrapper.firstChild) {
     firstWrapper.firstChild.remove();
   }
 
   secondWrapper.appendChild(templateClone);
+
+  trigger('title', 'green');
 });
